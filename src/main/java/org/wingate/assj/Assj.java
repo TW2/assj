1e9dd469b8f3b84897a1197aade4d0661a32720c
Yves <util2@msn.com>
2022-08-08 10:03:45 +0200

    Init

---
+++ src/main/java/org/wingate/assj/Assj.java
@@ -0,0 +1,12 @@
+package org.wingate.assj;
+
+/**
+ *
+ * @author util2
+ */
+public class Assj {
+
+    public static void main(String[] args) {
+        System.out.println("Hello World!");
+    }
+}
